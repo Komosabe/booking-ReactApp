48c7dd7db5a48de0411e4cb44ae049d44fa64a2e
Maciek Pietrasz <73303606+Komosabe@users.noreply.github.com>
2024-05-19 15:06:24 +0200

    feat: create hall form

---
+++ src/api-booking/user-panel/halls/createHall.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { API_URL } from "../../../misc/config/env";
+import { api } from "../../../lib/axios/config";
+
+export const createHallSchema = z.object({
+    name: z.string(),
+    capacity: z.number()
+});
+
+export type TCreateHallFormFields = z.infer<typeof createHallSchema>;
+export type TCreateHallResponse = {message: string};
+
+export const createHall = (data: TCreateHallFormFields) =>
+    api.post<TCreateHallResponse>(`${API_URL}/api/Hall/CreateHall`, data).then(({ data }) => data);
--- src/components/user-panel/halls/HallTitleWithButtonAndFormModal.tsx
+++ src/components/user-panel/halls/HallTitleWithButtonAndFormModal.tsx
@@ -1,6 +1,11 @@
 'use client'
 import { useState } from 'react'
-import { Button, Group, Modal, Title } from '@mantine/core'
+import { Button, Group, Modal, NumberInput, Stack, TextInput, Title } from '@mantine/core'
+import { useForm } from 'react-hook-form'
+import { TCreateHallFormFields, createHall, createHallSchema } from '../../../api-booking/user-panel/halls/createHall'
+import { zodResolver } from '@hookform/resolvers/zod'
+import toast from 'react-hot-toast'
+import { useMutation } from '@tanstack/react-query'
 
 export const HallTitleWithButtonAndFormModal = () => {
   const [modalOpened, setModalOpened] = useState(false)
@@ -20,10 +25,39 @@ type TAddHallFormModalProps = {
   onClose: () => void
 }
 
+
 const AddHallFormModal = ({ onClose, opened }: TAddHallFormModalProps) => {
+
+  const { register, handleSubmit } = useForm<TCreateHallFormFields>({
+    resolver: zodResolver(createHallSchema),
+  })
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: createHall,
+    onSuccess: () => {
+      toast.success('Hall created')
+      onClose()
+    },
+  })
+
+  const onsubmit = handleSubmit((data) => {
+    mutate(data)
+  })
+
   return (
     <Modal opened={opened} onClose={onClose} title="Nowe miejsce">
-      {/* Modal content */}
+      <form onSubmit={onsubmit}>
+        <Stack gap={'xs'}>
+          <TextInput {...register('name')} placeholder="Nazwa miejsca" />
+          <NumberInput {...register('capacity')} min={0} placeholder="Liczba miejsc" />
+          <Group w={"100%"} wrap={"nowrap"} gap={"xs"}>
+            <Button fullWidth color="blue" variant="outline" onClick={onClose}>
+              Anuluj
+            </Button>
+            <Button fullWidth color="blue" type='submit' disabled={isPending} loading={isPending}>Dodaj</Button>
+          </Group>
+        </Stack>
+      </form>
     </Modal>
   )
 }
